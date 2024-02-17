c7151254f540fcd39b8408b32ca1f438b7f5a466
Christopher Mackay <19520587+chris-mackay@users.noreply.github.com>
2024-02-17 17:06:27 -0500

    Update prog.py

--- prog.py
+++ prog.py
@@ -1,68 +1,46 @@
+# Import common language runtime
 import clr
-import os
 
+# Add references to the .Net libraries to access forms and controls
 clr.AddReference("System.Windows.Forms")
 clr.AddReference("System.Drawing")
 
 from System.Windows.Forms import *
 from System.Drawing import *
 
-dialog = OpenFileDialog()
-f = ""
-
 class MainForm(Form):
+
+    # Create the definition for the form
     def __init__(self):
+        
+        # Set properties of the form
         self.Text = "PyWinForms"
-        self.Width = 400
+        self.Width = 300
         self.Height = 200
-        self.ReSizable = False
+        self.Sizable = False
         self.MaximizeBox = False
         self.MinimizeBox = False
         self.ShowIcon = False
+
+        # Create a new button and set properties
+        button_show_message = Button()
+        button_show_message.Text = "Click me"
+        button_show_message.Location = Point(10, 10)
+        button_show_message.Height = 30
+        button_show_message.Width = 150
+
+        # Create a click event for the show message button
+        button_show_message.Click += self.buttonShowMessagePressed
+
+        # Add the show message button to the forms list of controls
+        self.Controls.Add(button_show_message)
+
+    # Create a definition for the click event
+    def buttonShowMessagePressed(self, sender, args):
+        MessageBox.Show("Hello from WinForms!", "PyWinForms", MessageBoxButtons.OK, MessageBoxIcon.Information)
         
-        label_filename = Label()
-        label_filename.Text = "Filename"
-        label_filename.Height = 13
-        label_filename.Width = 393
-        label_filename.Location = Point(10, 10)
-
-        self.textbox_filename = TextBox()
-        self.textbox_filename.Height = 20
-        self.textbox_filename.Width = 350
-        self.textbox_filename.Location = Point(10, 30)
-        self.textbox_filename.ReadOnly = True
-        self.textbox_filename.Enabled = True
-
-        button_select_file = Button()
-        button_select_file.Text = "Select file"
-        button_select_file.Location = Point(10, 60)
-        button_select_file.Height = 23
-        button_select_file.Width = 75
-
-        button_show_file = Button()
-        button_show_file.Text = "Open file"
-        button_show_file.Location = Point(90, 60)
-        button_show_file.Height = 23
-        button_show_file.Width = 75
-
-        button_select_file.Click += self.buttonSelectFilePressed
-        button_show_file.Click += self.buttonShowFilePressed
-
-        self.Controls.Add(label_filename)
-        self.Controls.Add(self.textbox_filename)
-        self.Controls.Add(button_select_file)
-        self.Controls.Add(button_show_file)
-
-    def buttonSelectFilePressed(self, sender, args):
-        if dialog.ShowDialog() == DialogResult.OK:
-            f = dialog.FileName
-            self.textbox_filename.Text = f
-
-    def buttonShowFilePressed(self, sender, args):
-        if self.textbox_filename.Text != "":
-            os.startfile(self.textbox_filename.Text)
-        else:
-            MessageBox.Show("No file to open.", "PyWinForms", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            
+# Create a new form
 form = MainForm()
+
+# Run the form
 Application.Run(form)
