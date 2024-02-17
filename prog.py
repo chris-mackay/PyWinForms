12641c1afa3c7ce710589735c88f3c5f5a0de9c6
Christopher Mackay <cmackay732@gmail.com>
2024-02-17 13:23:57 -0500

    Commit

--- prog.py
+++ prog.py
@@ -1,90 +1,68 @@
 import clr
-import time
-
-from time import sleep
+import os
 
 clr.AddReference("System.Windows.Forms")
 clr.AddReference("System.Drawing")
-clr.AddReference("RvtProgressBar")
 
 from System.Windows.Forms import *
 from System.Drawing import *
-from RvtProgressBar import *
 
 dialog = OpenFileDialog()
 f = ""
 
 class MainForm(Form):
     def __init__(self):
-        self.Text = "Data Import"
-        self.Height = 176
-        self.Width = 393
-        self.Sizable = False
+        self.Text = "PyWinForms"
+        self.Width = 400
+        self.Height = 200
+        self.ReSizable = False
         self.MaximizeBox = False
         self.MinimizeBox = False
         self.ShowIcon = False
         
         label_filename = Label()
-        label_filename.Text = "Select the csv file containing the data to import"
+        label_filename.Text = "Filename"
         label_filename.Height = 13
         label_filename.Width = 393
-        label_filename.Location = Point(13, 14)
+        label_filename.Location = Point(10, 10)
 
         self.textbox_filename = TextBox()
         self.textbox_filename.Height = 20
-        self.textbox_filename.Width = 266
-        self.textbox_filename.Location = Point(16, 35)
+        self.textbox_filename.Width = 350
+        self.textbox_filename.Location = Point(10, 30)
         self.textbox_filename.ReadOnly = True
-        self.textbox_filename.Enabled = False
+        self.textbox_filename.Enabled = True
 
         button_select_file = Button()
         button_select_file.Text = "Select file"
-        button_select_file.Location = Point(288, 32)
+        button_select_file.Location = Point(10, 60)
         button_select_file.Height = 23
         button_select_file.Width = 75
 
-        button_import = Button()
-        button_import.Text = "Import"
-        button_import.Location = Point(207, 102)
-        button_import.Height = 23
-        button_import.Width = 75
-
-        button_close = Button()
-        button_close.Text = "Close"
-        button_close.Location = Point(288, 102)
-        button_close.Height = 23
-        button_close.Width = 75
+        button_show_file = Button()
+        button_show_file.Text = "Open file"
+        button_show_file.Location = Point(90, 60)
+        button_show_file.Height = 23
+        button_show_file.Width = 75
 
         button_select_file.Click += self.buttonSelectFilePressed
-        button_import.Click += self.buttonImportPressed
-        button_close.Click += self.buttonClosePressed
+        button_show_file.Click += self.buttonShowFilePressed
 
         self.Controls.Add(label_filename)
         self.Controls.Add(self.textbox_filename)
         self.Controls.Add(button_select_file)
-        self.Controls.Add(button_import)
-        self.Controls.Add(button_close)
+        self.Controls.Add(button_show_file)
 
     def buttonSelectFilePressed(self, sender, args):
         if dialog.ShowDialog() == DialogResult.OK:
             f = dialog.FileName
             self.textbox_filename.Text = f
 
-    def buttonImportPressed(self, sender, args):
+    def buttonShowFilePressed(self, sender, args):
         if self.textbox_filename.Text != "":
-            if MessageBox.Show(f'Import data from?\n{self.textbox_filename.Text}', "Import data", MessageBoxButtons.YesNo, MessageBoxIcon.Question):
-                total = 10
-                pf = ProgressForm("Updating parameters", total)
-                for i in range(total):
-                    time.sleep(1)
-                    pf.Increment(total)
-                MessageBox.Show("Data import complete", "Import data", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                pf.Close()
+            os.startfile(self.textbox_filename.Text)
         else:
-            MessageBox.Show("Select a file containing the data to import", "Import data", MessageBoxButtons.OK, MessageBoxIcon.Error)
-
-    def buttonClosePressed(self, sender, args):
-        self.Close()
-
+            MessageBox.Show("No file to open.", "PyWinForms", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            
 form = MainForm()
 Application.Run(form)
